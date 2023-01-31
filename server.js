ce5d780e63b55b57df44895eff7e87345fb21050
rtecza <rafal.tecza@daftcode.pl>
2023-01-31 09:32:35 +0100

    Init

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/logo', (req, res) => {
+    res.sendFile(path.join(__dirname, 'assets', 'logo.png'));
+});
+
+app.listen(port, () => {
+    console.log(`Serwer uruchomiony na porcie ${port}`);
+});
