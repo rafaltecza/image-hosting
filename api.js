41c16183f552157e3c0f670cd5935d3f5cde991a
rtecza <rafal.tecza@daftcode.pl>
2023-02-02 09:35:40 +0100

    auth token

---
+++ api.js
@@ -0,0 +1,3 @@
+module.exports = {
+    alexisHRBaseUrl: process.env.ALEXIS_HR_BASE_URL
+};
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const path = require('path');
+const request = require('request');
+const requestProgress = require('request-progress');
+const Jimp = require('jimp');
+const async = require('async');
+const cron = require('node-cron');
+const dotenv = require('dotenv');
+const api = require('./api');
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -9,6 +18,7 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
 
@@ -19,3 +29,120 @@ app.get('/logo', (req, res) => {
 app.listen(port, () => {
     console.log(`Serwer uruchomiony na porcie ${port}`);
 });
+
+/**
+ * Cron job
+ * Runs every 15 minutes
+ **/
+cron.schedule('*/15 * * * *', () => {
+    getUsersList((err, users) => {
+        if (err) {
+            console.error(err);
+        } else {
+            users.forEach(user => {
+                getUserDetails(user.id, (err, userDetails) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        generateImage(userDetails)
+                            .then(message => console.log(message))
+                            .catch(err => console.error(err));
+                    }
+                });
+            });
+        }
+    });
+});
+
+const authToken = "Bearer " + process.env.AUTH_TOKEN;
+const alexisHRBaseUrl = api.alexisHRBaseUrl;
+
+/**
+ * Get list of users
+ * @param callback
+ */
+function getUsersList(callback) {
+    const options = {
+        url: alexisHRBaseUrl + "users",
+        headers: {
+            "Authorization": authToken
+        }
+    };
+
+    requestProgress(request(options, (error, response, body) => {
+        if (error) {
+            console.error(error);
+        }
+        const users = JSON.parse(body).users;
+        console.log(`Received list of ${users.length} users.`);
+        callback(null, users);
+    }), {
+        throttle: 1000,
+        delay: 1000
+    })
+        .on('progress', state => {
+            console.log(`Retrieving user list progress: ${(state.percent * 100).toFixed(2)}%`);
+        });
+}
+
+/**
+ * Get user details
+ * @param userId
+ * @param callback
+ */
+function getUserDetails(userId, callback) {
+    const options = {
+        url: alexisHRBaseUrl + `users/${userId}`,
+        headers: {
+            "Authorization": authToken
+        }
+    };
+
+    requestProgress(request(options, (error, response, body) => {
+        if (error) {
+            console.error(error);
+        }
+        const userDetails = JSON.parse(body);
+        console.log(`Received details for user ${userId}.`);
+        callback(null, userDetails);
+    }), {
+        throttle: 1000,
+        delay: 1000
+    })
+        .on('progress', state => {
+            console.log(`Retrieving user details for user ${userId} progress: ${(state.percent * 100).toFixed(2)}%`);
+        });
+}
+
+/**
+ * Generate image
+ * @param userDetails
+ * @returns {Promise<unknown>}
+ */
+function generateImage(userDetails) {
+    return new Promise((resolve, reject) => {
+        Jimp.read(userDetails.avatar)
+            .then(image => {
+                const text = new Jimp(image.bitmap.width, Jimp.AUTO, 0xFFFFFFFF);
+                text.print(Jimp.fonts.verdana10, 0, 0, {
+                    text: `Name: ${userDetails.first_name} ${userDetails.last_name}`,
+                    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+                }, image.bitmap.width);
+                text.print(Jimp.fonts.verdana10, 0, 15, {
+                    text: `Email: ${userDetails.email}`,
+                    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+                }, image.bitmap.width);
+                text.print(Jimp.fonts.verdana10, 0, 30, {
+                    text: `Title: ${userDetails.title}`,
+                    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+                }, image.bitmap.width);
+                image.composite(text, 0, image.bitmap.height);
+                return image
+                    .quality(100)
+                    .write(path.join(__dirname, `../public/generated/${userDetails.id}.png`), () => {
+                        resolve(`Image generated and saved to /public/generated/${userDetails.id}.png`);
+                    });
+            })
+            .catch(err => reject(err));
+    });
+}
