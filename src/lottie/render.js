16a48bde90e75ef9a8be9681ec740c3c7d57b90f
rtecza <rafal.tecza@daftcode.pl>
2023-02-21 12:45:02 +0100

    up

--- src/lottie/render.js
+++ src/lottie/render.js
@@ -1,6 +1,6 @@
 const renderLottie = require("puppeteer-lottie");
 
-const renderLottieAnimation = async (lottieAnimation, userUUID, fileName, extension = "mp4") => {
+const renderLottieAnimation = async (lottieAnimation, userUUID, fileName, extension = "gif") => {
     return await renderLottie({
         animationData: lottieAnimation,
         output: `public/generated/${userUUID}/${fileName}.${extension}`,
