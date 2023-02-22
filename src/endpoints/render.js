3a8dfa8507977f56c0a1e584bc98bc8495cc3047
rtecza <rafal.tecza@daftcode.pl>
2023-02-22 09:11:25 +0100

    upgrade

--- src/endpoints/render.js
+++ src/endpoints/render.js
@@ -25,31 +25,31 @@ const registerRender = (app) => {
                 fs.mkdirSync(path);
             }
             // const avatarAnimation = getAvatarItem(userUUID, employee?.avatarUrl);
-            const startDate = parseDate(employee?.break?.startDate)
-            const endDate = parseDate(employee?.break?.endDate)
-
-            const isCurrentDateBetweenVariable = isCurrentDateBetween(employee?.break?.startDate, employee?.break?.endDate);
-            const workingDate = isCurrentDateBetweenVariable ? endDate : startDate;
-            let imagePath = `assets/builder/status/${isCurrentDateBetweenVariable ? 'timesheet.svg' : 'upcoming.svg'}`
-
-            addTextToSVG(
-                imagePath,
-                {
-                    id: userUUID,
-                    firstName: employee?.firstName,
-                    month: workingDate.month,
-                    day: workingDate.day,
-                    year: workingDate.year
-                })
-                .then((modifiedSVG) => {
-                    console.log(modifiedSVG);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-
-            avatarAnimation.then(buffer => null)
-                .catch(err => null)
+            // const startDate = parseDate(employee?.break?.startDate)
+            // const endDate = parseDate(employee?.break?.endDate)
+            //
+            // const isCurrentDateBetweenVariable = isCurrentDateBetween(employee?.break?.startDate, employee?.break?.endDate);
+            // const workingDate = isCurrentDateBetweenVariable ? endDate : startDate;
+            // let imagePath = `assets/builder/status/${isCurrentDateBetweenVariable ? 'timesheet.svg' : 'upcoming.svg'}`
+            //
+            // addTextToSVG(
+            //     imagePath,
+            //     {
+            //         id: userUUID,
+            //         firstName: employee?.firstName,
+            //         month: workingDate.month,
+            //         day: workingDate.day,
+            //         year: workingDate.year
+            //     })
+            //     .then((modifiedSVG) => {
+            //         console.log(modifiedSVG);
+            //     })
+            //     .catch((error) => {
+            //         console.error(error);
+            //     });
+            //
+            // avatarAnimation.then(buffer => null)
+            //     .catch(err => null)
 
         })
 
